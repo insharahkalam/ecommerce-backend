62da4ef47949863ff33f83318d05d94149b1ca92
insharahkalam <insharahkalam19@gmail.com>
2026-08-14 16:33:29 +0500

    updated

--- src/router/notificationRoutes.js
+++ src/router/notificationRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Notification from "../models/Notification.js";
+import Notification from "../models/notifications.js";
 
 const router = express.Router();
 
--- src/utils/createNotification.js
+++ src/utils/createNotification.js
@@ -1,9 +1,9 @@
-import Notification from "../models/Notification.js";
+import notifications from "../models/notifications.js"
 import pusher from "../services/pusher.service.js";
 
 export const createNotification = async ({ type, title, message, link, relatedId }) => {
   try {
-    const notification = await Notification.create({ type, title, message, link, relatedId });
+    const notification = await notification.create({ type, title, message, link, relatedId });
 
     await pusher.trigger("admin-notifications", "new-notification", notification);
 
